1f5a9000280e5495e9bf05a554caa63921f99e0d
RVasiliy <repkinvasiliy@gmail.com>
2024-07-20 15:56:47 +0200

    feat: add "static" middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import Koa from 'koa';
+import serve from 'koa-static';
 import router from './router.js';
-
-const app = new Koa();
-
-app.use(router.routes());
 
 const PORT = 42301;
 const HOST = 'localhost';
+const PUBLIC_ROOT = resolve(dirname(fileURLToPath(import.meta.url)), '../public');
+
+const app = new Koa();
+
+app.use(serve(PUBLIC_ROOT, {})).use(router.routes());
 
 app.listen(PORT, HOST, () => {
   console.log(`Listening https://${HOST}:${PORT}/`);
