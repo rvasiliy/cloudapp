7014494a7c2793c718a56b92743b549c8d36afb3
RVasiliy <repkinvasiliy@gmail.com>
2024-07-21 13:31:17 +0200

    feat(page): add home page

---
+++ src/pages/home.js
@@ -0,0 +1,3 @@
+export default async function home(ctx) {
+    ctx.body = Math.random();
+}
--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 import Router from '@koa/router';
+import home from "./pages/home.js";
 
 const router = new Router();
+
+router.get('/', home);
 
 export default router;
