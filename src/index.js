9e13f0ce317df8812efef70dac8d0a6bc2972cd6
RVasiliy <repkinvasiliy@gmail.com>
2024-07-21 12:58:30 +0200

    build(config): install and config dotenv

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { dirname, resolve } from 'node:path';
+import { env } from 'node:process';
 import { fileURLToPath } from 'node:url';
 
+import 'dotenv/config.js';
 import Koa from 'koa';
 import serve from 'koa-static';
 import router from './router.js';
 
-const PORT = 42301;
-const HOST = 'localhost';
+const PORT = env.PORT || 8080;
+const HOST = env.HOST || 'localhost';
 const PUBLIC_ROOT = resolve(dirname(fileURLToPath(import.meta.url)), '../public');
 
 const app = new Koa();
