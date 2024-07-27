c67a24f30e795557694f47b771725202c066883d
RVasiliy <repkinvasiliy@gmail.com>
2024-07-27 12:16:24 +0200

    build(npm): install @sentry/browser

---
+++ src-ui/index.js
@@ -0,0 +1,6 @@
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+    dsn: "https://e4ce91c8ee6541816d15d5e06e4c0857@o4507616325074944.ingest.de.sentry.io/4507629320667216",
+    integrations: [],
+});
