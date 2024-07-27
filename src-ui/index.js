c979abc2227fa24e85a5fe457fcdcbae1594153f
RVasiliy <repkinvasiliy@gmail.com>
2024-07-27 17:36:31 +0200

    refactor: move sentry to common site layout

--- src-ui/index.js
+++ src-ui/index.js
@@ -1,13 +1,6 @@
-import * as Sentry from '@sentry/browser';
 import App from './app.js';
 
 import './styles.css';
-
-Sentry.init({
-  dsn: 'https://e4ce91c8ee6541816d15d5e06e4c0857@o4507616325074944.ingest.de.sentry.io/4507629320667216',
-  integrations: [],
-  release: APP_VERSION,
-});
 
 const app = new App();
 app.mount(document.querySelector('#app'));
